3c3d23167828358b4d29f38599fc4bf6f4453d8a
„Weronika <weronika.tabakowska@student.wat.edu.pl>
2020-11-23 23:57:59 +0100

    zaad221a

--- zaad 2-2/src/main.cpp
+++ zaad 2-2/src/main.cpp
@@ -1,37 +1,13 @@
 #include <Arduino.h>
+void setup() {
+		  // put your setup code here, to run once:
+		  pinMode(LED_BUILTIN, OUTPUT);
+		  digitalWrite(LED_BUILTIN, LOW);
+		}
+		
 
-uint8_t ledPin[] = {4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
-uint8_t ledDelay = 65;
-uint8_t direction = 1;
-uint8_t currentLED = 0;
-unsigned long changeTime;
-
-void changeLED();
-
-void setup(){
-  for (int i = 0; i < 10; i++){
-    pinMode(ledPin[i], OUTPUT);
-  }
-}
-
-void loop(){
-  if ((millis() - changeTime) > ledDelay){
-    changeLED();
-    changeTime = millis();
-  }
-}
-
-void changeLED(){
-  for (int x = 0; x < 10; x++){
-    digitalWrite(ledPin[x], LOW);
-  }
-  digitalWrite(ledPin[currentLED], HIGH);
-  currentLED += direction;
-  if (currentLED == 9) {
-    direction = -1;
-  }
-  if (currentLED == 0){
-    direction = 1;
-  }
-  
-}
+		void loop() {
+		  // put your main code here, to run repeatedly:
+		  digitalWrite(LED_BUILTIN, HIGH);
+		  digitalWrite(LED_BUILTIN, LOW);
+		} 
